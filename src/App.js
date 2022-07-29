eb84cd29c3fdbd116547fa5b5aa0f1cfa6b82d81
udaydevikar12 <uday.devikar@mediaagility.com>
2022-07-29 17:22:11 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import './App.css';
-import { Routes, Route } from "react-router-dom";
-import About from './components/About';
-import Contact from './components/Contact';
-
+import React from "react";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
       Home Page
-
+      <hr />
+      <div>
+        <button>
+          {" "}
+          <Link to={"/about"}>About</Link>
+        </button>
+        <button>
+          <Link to={"/contact"}>Contact</Link>
+        </button>
+      </div>
       <Routes>
         <Route path="/about" element={<About />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
       </Routes>
-
     </div>
   );
 }
